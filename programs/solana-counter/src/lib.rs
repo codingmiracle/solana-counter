368c3330ba2c000b5fd3844263b9b642ce06cd46
David <dav.gruber@htlstp.at>
2022-04-03 16:31:12 +0200

    program on devnet

---
+++ programs/solana-counter/src/lib.rs
@@ -0,0 +1,47 @@
+use anchor_lang::prelude::*;
+
+declare_id!("4DP7UpgsNPudn2f3yDZXGcf7yDPtcLvPYdvievPjck7W");
+
+#[program]
+pub mod solana_counter {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
